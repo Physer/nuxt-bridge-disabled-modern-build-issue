7043195d2f3c24cd3342d6624cad58fbf0620f47
Alex Schouls <alex.schouls@valtech.com>
2023-01-03 09:07:32 +0100

    Added Nuxt Bridge without Nuxi version

---
+++ nuxt-bridge-without-nuxi/externalFile.ts
@@ -0,0 +1,7 @@
+const data = {
+  id: "7a599e79-20aa-43af-a421-24b69ca18c84",
+  name: "random object",
+  description: `It doesn't matter`,
+};
+
+export default data;
---
+++ nuxt-bridge-without-nuxi/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{ myObject }}</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import data from '../externalFile' // Adding the '.ts' file extension will make the --modern build work
+
+export default Vue.extend({
+  name: 'IndexPage',
+  data: () => ({
+    myObject: data
+  })
+})
+</script>
