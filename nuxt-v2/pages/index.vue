563a7d28c136829b03e8662c127eccb76ef3b731
Alex Schouls <alex.schouls@valtech.com>
2023-01-03 08:57:02 +0100

    Added Nuxt V2 version

---
+++ nuxt-v2/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{ myObject }}</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import data from '../externalFile'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  data: () => ({
+    myObject: data
+  })
+})
+</script>
